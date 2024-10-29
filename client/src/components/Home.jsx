9d3a0cad1394d3f365a19b855bc98273826e511a
Zipory <ITZipory5@gmail.com>
2024-10-29 15:05:20 +0200

    fixed some bugs

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,8 @@
-
-import React from 'react';
+import React from "react";
 // import Header from './Header';
-import Footer from './home-page/Footer';
-import About from './home-page/About';
-import Logo from './Logo';
+import Footer from "./home-page/Footer";
+import About from "./home-page/About";
+import Logo from "./Logo";
 import WaitersEntry from "./home-page/WaitersEntry";
 import EmployersEntry from "./home-page/EmployersEntry";
 
@@ -15,18 +14,19 @@ const slogan = [
 
 const Home = () => {
   return (
-    <div >
-      <Logo/>
-      <main >
-          <h2>{slogan[Math.floor(Math.random() * slogan.length)]}</h2>
-      <h2>בר, הפלטפורמה שמחברת בין מעסיקים למלצרים!</h2>
-      <div className="entry-windows">
-        <WaitersEntry />
-        <EmployersEntry />
+    <div>
+      <Logo />
+      <main>
+        <h2>{slogan[Math.floor(Math.random() * slogan.length)]}</h2>
+        <h2>בר, הפלטפורמה שמחברת בין מעסיקים למלצרים!</h2>
+        <div className="entry-windows">
+          <WaitersEntry />
+          <EmployersEntry />
+          <About />
+        </div>
       </main>
-      <About/>
+    </div>
   );
 };
 
 export default Home;
-
