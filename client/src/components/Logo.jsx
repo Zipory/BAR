ddc74c6198b34ce50d4803a2f43b954328e6ce18
Zipory <ITZipory5@gmail.com>
2024-10-29 15:46:38 +0200

    tie img

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-
+import React from "react";
+const tie = require("../images/tie.png");
 const Logo = () => {
   return (
     <header>
-        <h1><img></img>AR</h1>
-        </header>
-  )
-}
+      <h1>
+        AR
+        <img
+          class="rotate-90"
+          src={tie}
+          alt="tie"
+          width="50px"
+          height="26px"
+          objectFit="cover"
+        />
+      </h1>
+    </header>
+  );
+};
 
-export default Logo
+export default Logo;
