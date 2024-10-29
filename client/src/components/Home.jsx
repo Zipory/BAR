5c4b175335b23f00e2bff2248b6802b5c0f8be77
BENI23LAHAV <beni0548472300@gmail.com>
2024-10-29 14:54:02 +0200

    waiters & Employers entry

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-// import Header from './Header';
-import Footer from './home-page/Footer';
-
+import React from "react";
+import Footer from "./home-page/Footer";
+import WaitersEntry from "./home-page/WaitersEntry";
+import EmployersEntry from "./home-page/EmployersEntry";
+const slogan = [
+  "תמיד חלמת לעבוד בזמנים שלך?",
+  "יש לך זמן פנוי שאתה רוצה לעבוד בו?",
+  "שובר את הראש בגיוס מלצרים בכל אירוע מחדש?",
+];
 const HomePage = () => {
   return (
-    <div >
-      <Header />
-      <main >
-        <h1 >Welcome to Our Website</h1>
-        <p >
-          We are glad to have you here. Explore our content and learn more about what we offer.
-        </p>
-        <button >Get Started</button>
-      </main>
+    <div>
+      <h2>{slogan[Math.floor(Math.random() * slogan.length)]}</h2>
+      <h2>בר, הפלטפורמה שמחברת בין מעסיקים למלצרים!</h2>
+      <div className="entry-windows">
+        <WaitersEntry />
+        <EmployersEntry />
+      </div>
       <Footer />
     </div>
   );
 };
-
 
 export default HomePage;
