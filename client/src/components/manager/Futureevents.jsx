d5ea3638c240fc6fa1b7ef518db9a0c54cbc990b
Zipory <ITZipory5@gmail.com>
2024-10-31 17:01:39 +0200

    starting manager dashbord

---
+++ client/src/components/manager/Futureevents.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Futureevents = () => {
+  return (
+    <div className="future-events">
+    <h3>אירועים עתידים:</h3>
+    <ul>
+      <li> אירוע 1: בית הנשיא ירושלים - 12/03/25  == 4/5 מלצרים</li>
+      <li>אירוע 2: כיכר החתולות ירושלים - 16/03/25  == 3/3 מלצרים</li>
+      <li>אירוע 3: רמדה רנסאנס ירושלים - 01/04/25  == 2/10 מלצרים</li>
+    </ul>
+  </div>
+  )
+}
+
+export default Futureevents;
