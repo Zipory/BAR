330f32289bb7cafef618f856eb294872c244317e
BENI23LAHAV <beni0548472300@gmail.com>
2024-11-06 14:31:25 +0200

    code organizied

--- server/app.js
+++ server/app.js
@@ -6,53 +6,81 @@ import jwt from "jsonwebtoken";
 import mysql from "mysql2";
 import { connection } from "./connection.js";
 import eventsRoutes from "./routes/eventsRoutes.js";
+import {
+  getCurrentDate,
+  getCurrentTime,
+  cutIsoDate,
+  sqlQueryInsert,
+  sqlQuerySelect,
+} from "./sources/function.js";
+import {
+  waitersFields,
+  employersFields,
+  eventsFields,
+  waiter_ditails,
+  employer_ditails,
+} from "./sources/variables.js";
 dotenv.config();
 // const { log } = require("console");
 // const bcrypt = require("bcrypt");
 const app = express();
 
 /**----------------Variables---------------- */
-const waitersFields = [
-  "first_name",
-  "last_name",
-  "phone",
-  "birthday",
-  "email",
-  "w_password",
-  "gender",
-  "avg_rating",
-  "status",
-];
+// const waitersFields = [
+//   "first_name",
+//   "last_name",
+//   "phone",
+//   "birthday",
+//   "email",
+//   "w_password",
+//   "gender",
+//   "avg_rating",
+//   "status",
+// ];
 
-const employersFields = [
-  "company_name",
-  "manager",
-  "manager_phone",
-  "email",
-  "e_password",
-  "about",
-  "avg_rating",
-  "status",
-];
+// const employersFields = [
+//   "company_name",
+//   "manager",
+//   "manager_phone",
+//   "email",
+//   "e_password",
+//   "about",
+//   "avg_rating",
+//   "status",
+// ];
 
-const eventsFields = [
-  "employer_fk",
-  "e_date",
-  "e_time",
-  "length",
-  "street",
-  "suite",
-  "event_description",
-  "waiters_sum",
-  "payment",
-  "is_global",
-  "has_sleep",
-];
+// const eventsFields = [
+//   "employer_fk",
+//   "e_date",
+//   "e_time",
+//   "length",
+//   "street",
+//   "suite",
+//   "event_description",
+//   "waiters_sum",
+//   "payment",
+//   "is_global",
+//   "has_sleep",
+// ];
 
-const employer_ditails =
-  "id , company_name , manager,manager_phone , email , about , avg_rating"; //Query for employer
-const waiter_ditails =
-  "first_name,last_name,phone,birthday,email,gender,avg_rating"; //Query for waiter
+// const employer_ditails = [
+//   "id",
+//   "company_name",
+//   "manager",
+//   "manager_phone",
+//   "email",
+//   "about",
+//   "avg_rating",
+// ]; //Query for employer
+// const waiter_ditails = [
+//   "first_name",
+//   "last_name",
+//   "phone",
+//   "birthday",
+//   "email",
+//   "gender",
+//   "avg_rating",
+// ]; //Query for waiter
 app.use(
   cors({
     origin: "*",
@@ -335,42 +363,42 @@ app.listen(port, () => {
 // limit number "number" - optional => but put 0
 // callback function - required
 
-function sqlQuerySelect(
-  selectWhat,
-  fromWhat,
-  whereCondition = [],
-  conditionChar = "=",
-  comparCondition = [],
-  limitNum = 0,
-  callback
-) {
-  let sql = `SELECT ${selectWhat} FROM ${fromWhat}`;
+// function sqlQuerySelect(
+//   selectWhat,
+//   fromWhat,
+//   whereCondition = [],
+//   conditionChar = "=",
+//   comparCondition = [],
+//   limitNum = 0,
+//   callback
+// ) {
+//   let sql = `SELECT ${selectWhat} FROM ${fromWhat}`;
 
-  // Adding WHERE conditions if provided
-  if (whereCondition.length > 0) {
-    sql += " WHERE ";
-    sql += whereCondition
-      .map((condition) => `${condition} ${conditionChar} ?`)
-      .join(" AND ");
-  }
+//   // Adding WHERE conditions if provided
+//   if (whereCondition.length > 0) {
+//     sql += " WHERE ";
+//     sql += whereCondition
+//       .map((condition) => `${condition} ${conditionChar} ?`)
+//       .join(" AND ");
+//   }
 
-  // Adding LIMIT clause if specified
-  if (Number(limitNum) > 0) {
-    sql += ` LIMIT ${limitNum}`;
-  }
-  console.log("sql query: ", sql);
+//   // Adding LIMIT clause if specified
+//   if (Number(limitNum) > 0) {
+//     sql += ` LIMIT ${limitNum}`;
+//   }
+//   console.log("sql query: ", sql);
 
-  // Executing the query
-  connection.query(sql, comparCondition, (err, results) => {
-    if (err) {
-      console.error(316, "Error fetching data:", err);
-      callback(err, null); // Call the callback with an error
-    } else {
-      // console.log(results);
-      callback(null, results); // Call the callback with results
-    }
-  });
-}
+//   // Executing the query
+//   connection.query(sql, comparCondition, (err, results) => {
+//     if (err) {
+//       console.error(316, "Error fetching data:", err);
+//       callback(err, null); // Call the callback with an error
+//     } else {
+//       // console.log(results);
+//       callback(null, results); // Call the callback with results
+//     }
+//   });
+// }
 
 //to use this function you must put this arguments:
 // table name "string" - required
@@ -378,54 +406,54 @@ function sqlQuerySelect(
 // values [array] - required
 // callback function- required
 //returns results of sql query in [{},{}...]
-function sqlQueryInsert(table, fields = [], values = [], callback) {
-  const placeholders = fields.map(() => "?").join(", ");
-  const sql = `INSERT INTO ${table} (${fields.join(
-    ", "
-  )}) VALUES (${placeholders})`;
+// function sqlQueryInsert(table, fields = [], values = [], callback) {
+//   const placeholders = fields.map(() => "?").join(", ");
+//   const sql = `INSERT INTO ${table} (${fields.join(
+//     ", "
+//   )}) VALUES (${placeholders})`;
 
-  connection.query(sql, values, (err, results) => {
-    if (err) {
-      console.error("Error inserting data:", err);
-      callback(err, null); // Call the callback with an error
-    } else {
-      callback(null, results); // Call the callback with results
-    }
-  });
-}
+//   connection.query(sql, values, (err, results) => {
+//     if (err) {
+//       console.error("Error inserting data:", err);
+//       callback(err, null); // Call the callback with an error
+//     } else {
+//       callback(null, results); // Call the callback with results
+//     }
+//   });
+// }
 //to use this function you must put this arguments:
 //date in "Date" type or string
 //returns current date in "string"
-function cutIsoDate(e_date) {
-  if (e_date instanceof Date) {
-    const year = e_date.getFullYear();
-    const month = String(e_date.getMonth() + 1).padStart(2, "0");
-    const day = String(e_date.getDate()).padStart(2, "0");
+// function cutIsoDate(e_date) {
+//   if (e_date instanceof Date) {
+//     const year = e_date.getFullYear();
+//     const month = String(e_date.getMonth() + 1).padStart(2, "0");
+//     const day = String(e_date.getDate()).padStart(2, "0");
 
-    return `${year}-${month}-${day}`;
-  } else {
-    console.warn("Warning: e_date is not a Date object. Value:", e_date);
-    e_date = String(e_date);
-    return e_date.slice(0, 10);
-  }
-}
+//     return `${year}-${month}-${day}`;
+//   } else {
+//     console.warn("Warning: e_date is not a Date object. Value:", e_date);
+//     e_date = String(e_date);
+//     return e_date.slice(0, 10);
+//   }
+// }
 //returns current date in "string"
-function getCurrentDate() {
-  const date = new Date();
+// function getCurrentDate() {
+//   const date = new Date();
 
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
+//   const year = date.getFullYear();
+//   const month = String(date.getMonth() + 1).padStart(2, "0");
+//   const day = String(date.getDate()).padStart(2, "0");
 
-  return `${year}-${month}-${day}`;
-}
-//returns current time in "string"
-function getCurrentTime() {
-  const date = new Date();
+//   return `${year}-${month}-${day}`;
+// }
+// //returns current time in "string"
+//  function getCurrentTime() {
+//   const date = new Date();
 
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
+//   const hours = String(date.getHours()).padStart(2, "0");
+//   const minutes = String(date.getMinutes()).padStart(2, "0");
+//   const seconds = String(date.getSeconds()).padStart(2, "0");
 
-  return `${hours}:${minutes}:${seconds}`;
-}
+//   return `${hours}:${minutes}:${seconds}`;
+// }
--- server/routes/eventsRoutes.js
+++ server/routes/eventsRoutes.js
@@ -4,11 +4,30 @@ import mysql from "mysql2";
 import express from "express";
 import { connection } from "../connection.js";
 import dotenv from "dotenv";
+import {
+  getCurrentDate,
+  getCurrentTime,
+  cutIsoDate,
+  sqlQueryInsert,
+  sqlQuerySelect,
+} from "../sources/function.js";
 dotenv.config();
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.status(200).send("hello");
+});
+router.post("/new-event", (req, res) => {
+  console.log(req.body);
+  const NewEvent = req.body;
+  const userToken = req.header("Authorization") || true;
+  const userEmail = req.header("email");
+
+  const userID = "";
+  if (userToken) {
+  } else {
+    res.status(401).send("Unauthorized");
+  }
 });
 
 export default router;
