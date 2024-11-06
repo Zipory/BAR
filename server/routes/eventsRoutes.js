d368145e12aabddb19af4ac4d7a062dbbab26084
BENI23LAHAV <beni0548472300@gmail.com>
2024-11-06 15:45:45 +0200

    new-event

--- server/routes/eventsRoutes.js
+++ server/routes/eventsRoutes.js
@@ -11,6 +11,13 @@ import {
   sqlQueryInsert,
   sqlQuerySelect,
 } from "../sources/function.js";
+import {
+  waitersFields,
+  employersFields,
+  eventsFields,
+  waiter_ditails,
+  employer_ditails,
+} from "../sources/variables.js";
 dotenv.config();
 const router = express.Router();
 
@@ -18,13 +25,55 @@ router.get("/", (req, res) => {
   res.status(200).send("hello");
 });
 router.post("/new-event", (req, res) => {
-  console.log(req.body);
-  const NewEvent = req.body;
+  //   console.log(req.body);
+  const newEvent = req.body;
   const userToken = req.header("Authorization") || true;
   const userEmail = req.header("email");
+  const eventsArray = [
+    newEvent.date,
+    newEvent.time,
+    Number(newEvent.len),
+    newEvent.street,
+    newEvent.suite,
+    newEvent.description,
+    Number(newEvent.waiters_sum),
+    Number(newEvent.payment),
+    newEvent.is_global,
+    newEvent.has_sleep,
+  ];
+  console.log("email: ", userEmail);
 
-  const userID = "";
   if (userToken) {
+    sqlQuerySelect(
+      "id",
+      "employers",
+      ["email"],
+      "=",
+      [userEmail],
+      0,
+      (err, results) => {
+        if (err) {
+          res.status(500).json("Error finding employer ID inside database");
+        } else {
+          //   res.status(200).send(JSON.stringify(results[0].id));
+          eventsArray.unshift(Number(results[0].id));
+          sqlQueryInsert(
+            "events",
+            eventsFields,
+            eventsArray,
+            (err, results) => {
+              if (err) {
+                res
+                  .status(500)
+                  .json("Error inserting new event into the database");
+              } else {
+                res.status(200).json("Event created successfully");
+              }
+            }
+          );
+        }
+      }
+    );
   } else {
     res.status(401).send("Unauthorized");
   }
