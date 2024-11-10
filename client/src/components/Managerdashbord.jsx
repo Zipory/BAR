b397309efa9b4229d243321c069cf5a72557d10a
Zipory <ITZipory5@gmail.com>
2024-11-10 15:32:33 +0200

    modal in new event

--- client/src/components/Managerdashbord.jsx
+++ client/src/components/Managerdashbord.jsx
@@ -1,15 +1,18 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import "../style/ManagerDashboard.css";
 import Info from "./manager/Info";
 import Futureevents from "./manager/Futureevents.jsx";
 import History from "./manager/History.jsx";
 import { FetchPost } from "./Fetch.js";
 import { userInfo } from "../App";
+import Newevent from "./manager/Newevent.jsx";
 const serverUrl = "http://localhost:4000/";
 const ManagerDashboard = () => {
   const [user, setUser] = useContext(userInfo);
   const [history, setHistory] = useState([]);
   const [showHistory, setShowHistory] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const divRef = useRef(null);
   useEffect(() => {
     if (history.length > 0) {
       setShowHistory(true);
@@ -18,15 +21,32 @@ const ManagerDashboard = () => {
   const getHistory = () => {
     FetchPost(serverUrl, { email: user.email }, setHistory);
   };
+
+  useEffect(() => {
+    // Attach event listener on component mount
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Clean up event listener on component unmount
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (event) => {
+    if (divRef.current && !divRef.current.contains(event.target)) {
+      console.log(27, "hi");
+      setShowModal(false);
+    }
+  }
   return (
     <div className="manager-dashboard">
       <Info />
 
       {/* Create New Event Button */}
-      <button className="big-button" onClick={() => alert("Create New Event")}>
+      <button className="big-button" onClick={() =>setShowModal(true)}>
         ליצירת אירוע חדש
       </button>
-
+      <div ref={divRef}> {showModal && <Newevent/>}</div>
+     
       {/* Future Events Window */}
       <Futureevents />
 
