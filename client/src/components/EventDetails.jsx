1072cb2e3a94263d477d5cee2ab749fbadfc9d10
Zipory <ITZipory5@gmail.com>
2024-11-11 17:49:29 +0200

    some small improvements

--- client/src/components/EventDetails.jsx
+++ client/src/components/EventDetails.jsx
@@ -6,17 +6,17 @@ const EventDetails = ({ eventInfo , company}) => {
   const urlDelete = "events/delete-event"
   return (
     <div className="event-details">
-      <button onClick={<Newevent/>}>Edit</button>
+      <button >Edit</button>
       <button onClick={() =>FetchDelete(urlDelete, company?.email, eventInfo)}>Delete</button>
       <h2>Event Details</h2>
       <p><strong>תאריך:</strong> {eventInfo.e_date}</p>
       <p><strong>זמן התחלה:</strong> {eventInfo.e_time}</p>
-      <p><strong>אורך האירוע:</strong> {eventInfo.length} שעות</p>
-      <p><strong>מיקום:</strong> {eventInfo.city} {eventInfo.street}, מס' {eventInfo.suite}</p>
+      <p><strong>אורך האירוע:</strong> {eventInfo.e_duration} שעות</p>
+      <p><strong>מיקום:</strong> {eventInfo.location}, מס' {eventInfo.suite}</p>
       <p><strong>תאור האירוע:</strong> {eventInfo.description || "No description provided"}</p>
-      <p><strong>Number of Waiters:</strong> {eventInfo.waitersSum}</p>
-      <p><strong>Payment:</strong> {eventInfo.payment} {eventInfo.globaly ? "Global" : "Hourly"}</p>
-      <p><strong>Includes Sleep:</strong> {eventInfo.sleep ? "Yes" : "No"}</p>
+      <p><strong>Number of Waiters:</strong> {eventInfo.waiters_amount}</p>
+      <p><strong>Payment:</strong> {eventInfo.salary} {eventInfo.is_global ? "Global" : "Hourly"}</p>
+      <p><strong>Includes Sleep:</strong> {eventInfo.has_sleep ? "Yes" : "No"}</p>
     </div>
   );
 };
