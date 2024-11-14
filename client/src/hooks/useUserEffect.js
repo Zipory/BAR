3a334852b8f40905cfcf06e02ff31ac4e2c376b5
Zipory <ITZipory5@gmail.com>
2024-11-14 14:15:15 +0200

    white page to waiter

--- client/src/hooks/useUserEffect.js
+++ client/src/hooks/useUserEffect.js
@@ -1,12 +1,16 @@
-import { useEffect } from "react";
-
+import { useEffect, useContext } from "react";
+import { typeOfUser } from "../App";
 function useUserEffect(state, navigate, navTo) {
+  const [isAwaiter, setIsAwaiter] = useContext(typeOfUser);
   useEffect(() => {
   
     console.log("State has changed:", state);
-    if (state !== null && state !== false) navigate(navTo);
+    if (state !== null && state !== false) {
+      isAwaiter === true ?  navigate(navTo.waiter) :  navigate(navTo.manager);
+
+      // navigate(navTo);
     // This effect will run only when 'state' changes
-    
+    }
   }, [state]);
 }
 
