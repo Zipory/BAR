cc884cab2e654109b487a577266dcb11b547ac91
Zipory <ITZipory5@gmail.com>
2024-11-20 17:53:50 +0200

    waiter get a good fetch in past, pending, future, all_events

--- client/src/components/WaiterDashboard.jsx
+++ client/src/components/WaiterDashboard.jsx
@@ -2,36 +2,37 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import UserInfo from "./waiter/info";
 import Allevents from "./waiter/Allevents";
-// import Futureevents from "./waiter/FutureeEvent";
-import FutureEvents from "./waiter/FutureEvents"
+import FutureEvents from "./waiter/FutureEvents";
 import Pastevents from "./waiter/Pastevents";
 import PendingEvents from "./waiter/pendingEvents";
-import "../style/waiterDashboard.css"
+import "../style/waiterDashboard.css";
 
 const WaiterDashboard = () => {
-  let isAwaiter2 =JSON.parse( window.localStorage.getItem("isWaiter"));
+  let isAwaiter2 = JSON.parse(window.localStorage.getItem("isWaiter"));
   const navigate = useNavigate();
   useEffect(() => {
- if (!isAwaiter2) {
-    console.log("not a waiter");
-    navigate("/home");
-  }
+    if (!isAwaiter2) {
+      console.log("not a waiter");
+      navigate("/home");
+    }
   }, []);
- 
+
   return (
     <div>
-      <h2><UserInfo/></h2>
-      <section>אירועים עתידיים כלליים:
+      <h2>
+        <UserInfo />
+      </h2>
+      {/* <section>אירועים עתידיים כלליים:
         <Allevents/>
-      </section>
+      </section> */}
       {/* <section>אירועים עתידיים פרטיים:
-        <Futureevents/>
+        <FutureEvents/>
+      </section> */}
+      {/* <section>
+        אירועים שעברו פרטיים: <Pastevents />
       </section> */}
       <section>
-        {/* <Pastevents/>
         <PendingEvents/>
-       */}
-         {/* <FutureEvents/> */}
       </section>
     </div>
   );
