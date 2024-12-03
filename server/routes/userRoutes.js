6db79c7aa03361abbb9b84875940a697b488e8fa
BENI23LAHAV <beni0548472300@gmail.com>
2024-12-03 13:40:51 +0200

    delete passwords from user/info

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,31 +5,78 @@ import { sendMail } from "../Gmail sender/send.js";
 import {
   authenticateToken,
   extractingUserDetails,
+  cutIsoDate,
 } from "../sources/function.js";
 dotenv.config();
 const router = express.Router();
 //get user info
 async function getUser(req, res) {
-  const user = await extractingUserDetails(req.headers["authorization"]);
-  if (!user)
+  try {
+    const user = await extractingUserDetails(req.headers["authorization"]);
+    if (!user)
+      return res
+        .status(401)
+        .json({ message: "Access denied", succeed: false, data: {} });
+    let results = await pool.query(
+      `SELECT * FROM ${user.isAwaiter ? "waiters" : "companies"} WHERE id = ?`,
+      [user.id]
+    );
+
+    results = results[0][0];
+    delete results[user.isAwaiter ? "w_password" : "e_password"];
+    if (user.isAwaiter) {
+      results.birthday = cutIsoDate(results.birthday);
+    }
     return res
-      .status(401)
-      .json({ message: "Access denied", succeed: false, data: {} });
-  const results = await pool.query(
-    `SELECT * FROM ${user.isAwaiter ? "waiters" : "companies"} WHERE id = ?`,
-    [user.id]
-  );
-  return res
-    .status(200)
-    .json({ message: "User details", succeed: true, data: results[0][0] });
+      .status(200)
+      .json({ message: "User details", succeed: true, data: results });
+  } catch (err) {
+    console.log(err);
+
+    return res
+      .status(500)
+      .json({ message: "Something went wrong", succeed: false, data: [] });
+  }
 }
 
 router.get("/info", authenticateToken, getUser);
 
-//update user
-// async function updateUser(req, res) {
+async function companyDetails(req, res) {
+  try {
+    const companyID = req.params.companyID;
+    if (typeof Number(companyID) !== "number") {
+      return res.status(400).json({
+        message: "Please enter a valid company ID",
+        succeed: false,
+        data: {},
+      });
+    }
+    let results = await pool.query(
+      `SELECT * FROM companies WHERE id = ? LIMIT 1;`,
+      [companyID]
+    );
 
-// }
-// router.put("/update", authenticateToken, updateUser);
+    if (results[0].length === 0) {
+      return res.status(400).json({
+        message: "There is no company with this ID",
+        succeed: false,
+        data: {},
+      });
+    }
+    results = results[0][0];
+    delete results.e_password;
+    return res.status(200).json({
+      message: "Company details",
+      succeed: true,
+      data: results,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: err.message, succeed: false, data: [] });
+  }
+}
+
+router.get("/company-details/:companyID", authenticateToken, companyDetails);
 
 export default router;
