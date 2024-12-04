bc4f0fbaffd9850507984cdc4870e4e5283c06dc
Zipory <ITZipory5@gmail.com>
2024-12-04 15:13:27 +0200

    more flexebility to the events-list

--- client/src/components/manager/Calender.jsx
+++ client/src/components/manager/Calender.jsx
@@ -3,6 +3,7 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import heLocale from '@fullcalendar/core/locales/he';
 import "../../style/calander.css"
 function Calendar({setDate}) {
   const [events, setEvents] = useState([]);
@@ -26,6 +27,19 @@ function Calendar({setDate}) {
         events={events}  // Display events
         selectable={true}  // Allow date selection
         dateClick={(info) =>  setDate(info.dateStr)}  // Example interaction
+        locales={[heLocale]}  // Add the Hebrew locale
+        locale='he'  // Set the calendar to use Hebrew
+        // headerToolbar={{
+        //   left: 'prev,next today',
+        //   center: 'title',
+        //   right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        // }}
+        Responsive breakpoints
+        windowResize={(arg) => {
+          if (arg.view.type === 'dayGridMonth' && window.innerWidth < 768) {
+            arg.view.calendar.changeView('timeGridDay');
+          }
+        }}
       />
     </div>
   );
