777d390a9cb95274856ed3fe34e120e6c396c8a7
Zipory <ITZipory5@gmail.com>
2024-12-12 12:31:51 +0200

    cloudinary ok on client

--- client/src/components/waiter/profile/CloudinaryHandle.jsx
+++ client/src/components/waiter/profile/CloudinaryHandle.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { CloudinaryContext, Image, Transformation } from 'cloudinary-react';
+
+const preset = process.env.REACT_APP_CLOUDINARY_PRESET
+const cloudinaryName = process.env.REACT_APP_CLOUDINARY_NAME
+
 const CloudinaryHandle = ({setCloudinaryUrl}) => {
   const [file, setFile] = useState(null);
 
@@ -12,30 +16,15 @@ const CloudinaryHandle = ({setCloudinaryUrl}) => {
     e.preventDefault();
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("upload_preset", "my_preset"); // replace with your Cloudinary upload preset
-
+    formData.append("upload_preset", preset); 
     try {
       const response = await axios.post(
-        "https://api.cloudinary.com/v1_1/dfrcalpyz/image/upload",
+        `https://api.cloudinary.com/v1_1/${cloudinaryName}/image/upload`,
         formData
       );
-      // the real url of the img
+      // the url of the img
       const imageUrl = response.data.secure_url;
       setCloudinaryUrl(()=> imageUrl);
-      // the url of the trasaction img.
-      // setCloudinaryUrl(`${response.data.secure_url}?c_thumb&g_face&h=200&w=200`);
-      // const cloudinaryUrl = `${response.data.secure_url}?w=300&h=300&c_fill`;
-      console.log(imageUrl);
-      
-      // Send the image URL to your server
-      // await axios.post("/your-server-endpoint", { imageUrl });
-      //   width: 200,
-      // height: 200,
-      // crop: "thumb",
-      // gravity: "face"
-      // console.log(response);
-      // https://res.cloudinary.com/dfrcalpyz/image/upload/f_auto,q_auto/c_thumb,g_face,h_200,w_200/iph54muyqhugkiitbe4e?_a=BAMCkGXw0
-      
     } catch (error) {
       console.error("Error uploading image", error);
     }
